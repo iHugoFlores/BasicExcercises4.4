ce8838b3cb079226db70b2af3ab49e2ac8f6613d
Hugo Flores <hfp.ing2@gmail.com>
2020-04-04 13:26:37 -0400

    Base Project

---
+++ PassingBackDelegationSwift/PassingBackDelegationSwift/ReceiverVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingBackDelegationSwift
+//
+//  Created by Hugo Flores Perez on 4/4/20.
+//  Copyright © 2020 Hugo Flores Perez. All rights reserved.
+//
+
+import UIKit
+
+class ReceiverVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
